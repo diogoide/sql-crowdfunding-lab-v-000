1256503427131c0bdcca0354e0d05db6ac7deab2
Diogo Ide <diogoide@gmail.com>
2018-09-16 18:04:01 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,7 @@ end
 
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"SELECT users.name, users.age SUM(pledges.amount)
+"SELECT users.name SUM(pledges.amount)
 FROM projects
 INNER JOIN pledges
 ON pledges.project_id = projects.id
