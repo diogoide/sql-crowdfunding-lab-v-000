e913c0e12cc485bbfb4b67894ffd8e2b05c51c60
Diogo Ide <diogoide@gmail.com>
2018-09-16 17:43:55 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -9,7 +9,7 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
   "SELECT Projects.title, Pledges.amount FROM projects
   LEFT JOIN pledges ON Pledges.project_id = Projects.id
-  ORDER BY Projects.title;"
+  ORDER BY Projects.title ASC;"
 end
 
 
